ea9d451895c9ba241df4846bb8be382db796b751
Miraii133 <jiyovalmoria@gmail.com>
2024-11-13 09:43:22 +0800

    chore: cleanup prisma code.

--- lib/helper/prisma.dart
+++ lib/helper/prisma.dart
@@ -2,45 +2,15 @@ import 'dart:developer';
 
 import 'package:flutter/widgets.dart';
 import 'package:orm_flutter/orm_flutter.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
 
 import 'package:audio_classification/prisma/generated_dart_client/client.dart';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 late final PrismaClient prisma;
-
-/* Future<void> initPrismaClient() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await dotenv.load(); // Load .env file
-  final directory = await getApplicationDocumentsDirectory();
-  final dbPath = '${directory.path}/basketball_stats.db';
-
-  final dbUrl =
-      dotenv.env['DATABASE_URL']?.replaceFirst('PLACEHOLDER_PATH', dbPath);
-  final prisma = PrismaClient();
-  final engine = prisma.$engine as LibraryEngine;
-
-  await engine.applyMigrations(
-    path:
-        'prisma/migrations/', // You define in `flutter.assets` migrations root dir
-  );
-
-  await prisma.$connect();
-  log('prisasdasdma ${prisma}');
-}
- */
 
 Future<void> initPrismaClient() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  final supportDir = await getApplicationSupportDirectory();
-  final database =
+  const database =
       '/data/data/org.tensorflow.audio_classification.audio_classification/databases/basketball_stats.db';
-  //await dotenv.load(); // Ensure dotenv is loaded first
-  //final databaseUrl = dotenv.env['DATABASE_URL']!;
   prisma = PrismaClient(datasourceUrl: 'file:${database}');
   final engine = switch (prisma.$engine) {
     LibraryEngine engine => engine,
@@ -49,6 +19,5 @@ Future<void> initPrismaClient() async {
 
   await prisma.$connect();
   await engine?.applyMigrations(path: 'lib/prisma/migrations');
-
-  print('migrations?');
+  log('Applied migrations');
 }
